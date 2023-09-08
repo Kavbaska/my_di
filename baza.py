bf0a621949cce209124dffbeb3450458b7c7de88
Kavbaska <van53860727@yandex.ru>
2023-09-08 19:53:20 +0300

    2 vers

--- baza.py
+++ baza.py
@@ -1,5 +1,5 @@
 import requests
 
-URL = 'https://67d7073b-4eae-42a1-a4e5-10107188605c.serverhub.praktikum-services.ru'
+URL = 'https://6b92f5e6-ecb9-49d3-8d4c-0e2327a56710.serverhub.praktikum-services.ru'
 CREAT_ZAKAZ = '/api/v1/orders'
 ZAPROS_ZAKAZ = '/api/v1/orders/track?t='
--- function.py
+++ function.py
@@ -1,22 +1,22 @@
 import requests
 import baza
 import data
-
-def new_zakaz(body):
+# Гапошко Иван 8-когорта  — Финальный проект. Инженер по тестированию плюс
+def test_new_zakaz(body):
     return requests.post(baza.URL + baza.CREAT_ZAKAZ,  # подставляем полный url
                          json=body,
                          headers=data.headers)
-    proverka_cod = new_zakaz()
+    proverka_cod = test_new_zakaz()
     assert proverka_cod.status_code == 200
-response = new_zakaz(data.zakaz);
+response = test_new_zakaz(data.zakaz);
 track = str(response.json()["track"])
 print(response.status_code)
 print(response.json())
 print(track)
 
 
-def zakaz_po_nomeru():
+def test_zakaz_po_nomeru():
     return requests.get(baza.URL + baza.ZAPROS_ZAKAZ + track,
                         headers=data.headers)
-response = zakaz_po_nomeru();
+response = test_zakaz_po_nomeru();
 print(response.json())
