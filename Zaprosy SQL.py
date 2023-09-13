d6f3cc141629d5baf14779e202e358b6fa12f4fb
Kavbaska <van53860727@yandex.ru>
2023-09-13 18:00:16 +0300

    pravki posle reviu

--- Zaprosy SQL.py
+++ Zaprosy SQL.py
@@ -1,15 +1,15 @@
-# SELECT c.id, COUNT(o.inDelivery)
+# SELECT c.login, COUNT(*) AS or_num
 # FROM "Orders" AS o
 # INNER JOIN "Couriers" AS c ON o.courierId = c.id
 # WHERE o.inDelivery = true
-# GROUP BY c.id;
+# GROUP BY c.login;
 #
 #
 # SELECT track,
 # 	CASE
-# 		WHEN finished == true
-# 		WHEN canсelled == true
-# 		WHEN inDelivery == true
+# 		WHEN finished = true THEN '2'
+# 		WHEN canсelled = true THEN '-1'
+# 		WHEN inDelivery = true THEN '1'
 # 		ELSE 0
 # 	END AS status
 # FROM "Orders";
